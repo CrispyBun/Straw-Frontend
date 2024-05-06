1aa21daeec601c4bae953a284f517e4d841e0533
CrispyBun <74562111+CrispyBun@users.noreply.github.com>
2024-05-06 23:41:02 +0200

    Restructure code

--- modules/api.js
+++ modules/api.js
@@ -1,3 +1,13 @@
 const url = 'http://localhost:3000';
 
-export { url }
+const boardsUrl = url + "/board?";
+const getBoards = async (skip = 0, limit = 10) => {
+    const response = await fetch(boardsUrl + new URLSearchParams({
+        skip: skip,
+        limit: limit
+    }));
+    const boardResponse = await response.json();
+    return boardResponse.data;
+}
+
+export { url, getBoards }
--- modules/loadBoardList.js
+++ modules/loadBoardList.js
@@ -1,28 +1,18 @@
 // Needs a #board-card-template (to be cloned)
 // Needs a .board-list (to put the clones into)
 
-import { url } from './api.js';
+import { getBoards } from './api.js';
 
 const boardCardTemplate = document.querySelector("#board-card-template");
 const list = document.querySelector(".board-list");
 
-const boardsUrl = url + "/board?";
-(async () => {
-    const boardRequest = await fetch(boardsUrl + new URLSearchParams({
-        skip: 0,
-        limit: 10
-    })).then((response) => {
-        return response.json();
-    });
+const boardData = await getBoards(0, 10);
 
-    const boardData = boardRequest.data;
-    
-    for (const board of boardData) {
-        const node = boardCardTemplate.content.cloneNode(true);
-        node.querySelector(".title").textContent = board.name;
-        node.querySelector(".summary").textContent = board.summary;
-        list.appendChild(node);
-    }
-})();
+for (const board of boardData) {
+    const node = boardCardTemplate.content.cloneNode(true);
+    node.querySelector(".title").textContent = board.name;
+    node.querySelector(".summary").textContent = board.summary;
+    list.appendChild(node);
+}
 
 // list.appendChild(boardCardTemplate.content.cloneNode(true));
