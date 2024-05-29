f2b78c6f946b6c825da6e9cce9aa6875497cc955
CrispyBun <74562111+CrispyBun@users.noreply.github.com>
2024-05-29 21:57:04 +0200

    Make navbar hide button work

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -4,11 +4,61 @@ const navbarLinkAccount = document.querySelector("#navbar-link-account");
 const navbarLinkProfile = document.querySelector("#navbar-link-profile");
 const navbarLinkPost = document.querySelector("#navbar-link-post");
 const navbarLinkChat = document.querySelector("#navbar-link-chat");
-const navbarLinkHome = document.querySelector("#navbar-link-home");
 
+// Toggle navbar links based on login
 if (!isLoggedIn()) {
     if (navbarLinkProfile) navbarLinkProfile.style.display = "none";
     if (navbarLinkPost) navbarLinkPost.style.display = "none";
     if (navbarLinkChat) navbarLinkChat.style.display = "none";
     if (navbarLinkAccount) navbarLinkAccount.href = "/login";
+}
+
+// Navbar hide button functionality
+
+const openNavbar = (navbar, skipAnimation) => {
+    if (skipAnimation) {
+        navbar.style.transition = "none";
+        navbarButton.style.transition = "none";
+    }
+
+    navbar.style.left = "-77px";
+    navbarButton.style.left = "77px";
+    navbarButton.style.transform = "rotate(180deg)";
+}
+
+const closeNavbar = (navbar, skipAnimation) => {
+    if (skipAnimation) {
+        navbar.style.transition = "none";
+        navbarButton.style.transition = "none";
+    }
+
+    navbar.style.left = null;
+    navbarButton.style.position = null;
+    navbarButton.style.left = null;
+    navbarButton.style.transform = null;
+}
+
+const navbar = document.querySelector("#navbar");
+const navbarButton = document.querySelector("#navbar-button-back");
+
+let navbarClosed = sessionStorage.getItem("navbarIsClosed") === "true";
+
+if (navbar && navbarButton) {
+    if (navbarClosed) closeNavbar(navbar, true);
+    else openNavbar(navbar, true);
+    
+    navbarButton.addEventListener("click", () => {
+        navbar.style.transition = null;
+        navbarButton.style.transition = null;
+
+        if (!navbarClosed) {
+            closeNavbar(navbar);
+            navbarClosed = true;
+        }
+        else {
+            openNavbar(navbar);
+            navbarClosed = false;
+        }
+        sessionStorage.setItem("navbarIsClosed", navbarClosed);
+    })
 }
